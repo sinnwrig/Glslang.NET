10868574751df51f058f94745b892c63dfcbbce9
Kai Angulo <kangriebling@gmail.com>
2024-10-21 02:40:03 -0700

    refactor: change to fixed struct pointers for Shader and Program

--- Source/GlslangNative.cs
+++ Source/GlslangNative.cs
@@ -4,6 +4,10 @@ using System.Runtime.CompilerServices;
 namespace Glslang.NET;
 
 
+internal unsafe struct NativeShader { }
+internal unsafe struct NativeProgram { }
+
+
 internal static partial class GlslangNative
 {
     const string LibName = "glslang";
@@ -22,143 +26,146 @@ internal static partial class GlslangNative
 
     [LibraryImport(LibName, EntryPoint = "glslang_shader_create")]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial IntPtr CreateShader(IntPtr input);
+    internal static unsafe partial NativeShader* CreateShader(IntPtr input);
 
 
     [LibraryImport(LibName, EntryPoint = "glslang_shader_delete")]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial void DeleteShader(IntPtr shader);
+    internal static unsafe partial void DeleteShader(NativeShader* shader);
 
 
 
     [LibraryImport(LibName, EntryPoint = "glslang_shader_set_preamble")]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial void SetShaderPreamble(IntPtr shader, IntPtr preamble);
+    internal static unsafe partial void SetShaderPreamble(NativeShader* shader, IntPtr preamble);
 
 
     [LibraryImport(LibName, EntryPoint = "glslang_shader_shift_binding")]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial void ShiftShaderBinding(IntPtr shader, ResourceType res, uint shiftBase);
+    internal static unsafe partial void ShiftShaderBinding(NativeShader* shader, ResourceType res, uint shiftBase);
 
 
     [LibraryImport(LibName, EntryPoint = "glslang_shader_shift_binding_for_set")]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial void ShiftShaderBindingForSet(IntPtr shader, ResourceType res, uint shiftBase, uint set);
+    internal static unsafe partial void ShiftShaderBindingForSet(NativeShader* shader, ResourceType res, uint shiftBase, uint set);
 
 
     [LibraryImport(LibName, EntryPoint = "glslang_shader_set_options")]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial void SetShaderOptions(IntPtr shader, ShaderOptions options);
+    internal static unsafe partial void SetShaderOptions(NativeShader* shader, ShaderOptions options);
 
 
     [LibraryImport(LibName, EntryPoint = "glslang_shader_set_glsl_version")]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial void SetShaderGLSLVersion(IntPtr shader, int version);
+    internal static unsafe partial void SetShaderGLSLVersion(NativeShader* shader, int version);
 
 
     [LibraryImport(LibName, EntryPoint = "glslang_shader_preprocess")]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int PreprocessShader(IntPtr shader, IntPtr input);
+    internal static unsafe partial int PreprocessShader(NativeShader* shader, IntPtr input);
 
 
     [LibraryImport(LibName, EntryPoint = "glslang_shader_parse")]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int ParseShader(IntPtr shader, IntPtr input);
+    internal static unsafe partial int ParseShader(NativeShader* shader, IntPtr input);
 
 
     [LibraryImport(LibName, EntryPoint = "glslang_shader_get_preprocessed_code")]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial IntPtr GetPreprocessedShaderCode(IntPtr shader);
+    internal static unsafe partial IntPtr GetPreprocessedShaderCode(NativeShader* shader);
 
 
     [LibraryImport(LibName, EntryPoint = "glslang_shader_get_info_log")]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial IntPtr GetShaderInfoLog(IntPtr shader);
+    internal static unsafe partial IntPtr GetShaderInfoLog(NativeShader* shader);
 
 
     [LibraryImport(LibName, EntryPoint = "glslang_shader_get_info_debug_log")]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial IntPtr GetShaderInfoDebugLog(IntPtr shader);
+    internal static unsafe partial IntPtr GetShaderInfoDebugLog(NativeShader* shader);
 
 
     // glslang_program_t
 
     [LibraryImport(LibName, EntryPoint = "glslang_program_create")]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial IntPtr CreateProgram();
+    internal static unsafe partial NativeProgram* CreateProgram();
 
 
     [LibraryImport(LibName, EntryPoint = "glslang_program_delete")]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial void DeleteProgram(IntPtr program);
+    internal static unsafe partial void DeleteProgram(NativeProgram* program);
 
 
 
     [LibraryImport(LibName, EntryPoint = "glslang_program_add_shader")]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial void AddShaderToProgram(IntPtr program, IntPtr shader);
+    internal static unsafe partial void AddShaderToProgram(NativeProgram* program, NativeShader* shader);
 
 
     [LibraryImport(LibName, EntryPoint = "glslang_program_link")]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int LinkProgram(IntPtr program, MessageType messages);
+    internal static unsafe partial int LinkProgram(NativeProgram* program, MessageType messages);
 
 
     [LibraryImport(LibName, EntryPoint = "glslang_program_add_source_text")]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial void AddProgramSourceText(IntPtr program, ShaderStage stage, IntPtr text, nuint len);
+    internal static unsafe partial void AddProgramSourceText(NativeProgram* program, ShaderStage stage, IntPtr text, nuint len);
 
 
     [LibraryImport(LibName, EntryPoint = "glslang_program_set_source_file")]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial void SetProgramSourceFile(IntPtr program, ShaderStage stage, IntPtr file);
+    internal static unsafe partial void SetProgramSourceFile(NativeProgram* program, ShaderStage stage, IntPtr file);
 
 
     [LibraryImport(LibName, EntryPoint = "glslang_program_map_io")]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int MapProgramIO(IntPtr program);
+    internal static unsafe partial int MapProgramIO(NativeProgram* program);
+
+
+    [LibraryImport(LibName, EntryPoint = "glslang_program_get_info_log")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static unsafe partial IntPtr GetProgramInfoLog(NativeProgram* program);
+
+
+    [LibraryImport(LibName, EntryPoint = "glslang_program_get_info_debug_log")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static unsafe partial IntPtr GetProgramInfoDebugLog(NativeProgram* program);
 
 
     // SPIR-V generation
 
     [LibraryImport(LibName, EntryPoint = "glslang_program_SPIRV_generate")]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial void GenerateProgramSPIRV(IntPtr program, ShaderStage stage);
+    internal static unsafe partial void GenerateProgramSPIRV(NativeProgram* program, ShaderStage stage);
 
 
     [LibraryImport(LibName, EntryPoint = "glslang_program_SPIRV_generate_with_options")]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial void GenerateProgramSPIRVWithOptiosn(IntPtr program, ShaderStage stage, IntPtr spvOptions);
+    internal static unsafe partial void GenerateProgramSPIRVWithOptiosn(NativeProgram* program, ShaderStage stage, IntPtr spvOptions);
 
 
     [LibraryImport(LibName, EntryPoint = "glslang_program_SPIRV_get_size")]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial nuint GetProgramSPIRVSize(IntPtr program);
+    internal static unsafe partial nuint GetProgramSPIRVSize(NativeProgram* program);
 
 
     [LibraryImport(LibName, EntryPoint = "glslang_program_SPIRV_get")]
     [UnmanagedCallConv(CallConvs = new Type[] { typeof(CallConvCdecl) })]
-    internal static partial void GetProgramSPIRVBuffer(IntPtr program, [Out] byte[] buffer); // Output type is actually uint, so buffer must be 4x the size
+    internal static unsafe partial void GetProgramSPIRVBuffer(NativeProgram* program, [Out] byte[] buffer); // Output type is actually uint, so buffer must be 4x the size
 
 
     [LibraryImport(LibName, EntryPoint = "glslang_program_SPIRV_get_ptr")]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial IntPtr GetProgramSPIRVBuffer(IntPtr program); // Allocates a buffer we have to free. Prefer SPIRV_get to this.
+    internal static unsafe partial IntPtr GetProgramSPIRVBuffer(NativeProgram* program); // Allocates a buffer we have to free. Prefer SPIRV_get to this.
+
+
 
 
     [LibraryImport(LibName, EntryPoint = "glslang_program_SPIRV_get_messages")]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial IntPtr GetProgramSPIRVMessages(IntPtr program);
+    internal static unsafe partial IntPtr GetProgramSPIRVMessages(NativeProgram* program);
 
-
-    [LibraryImport(LibName, EntryPoint = "glslang_program_SPIRV_get_info_log")]
-    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial IntPtr GetProgramInfoLog(IntPtr program);
-
-
-    [LibraryImport(LibName, EntryPoint = "glslang_program_SPIRV_get_info_debug_log")]
-    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial IntPtr GetProgramInfoDebugLog(IntPtr program);
 
 
     [LibraryImport(LibName, EntryPoint = "glslang_SPIRV_disassemble")]
